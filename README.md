349e3976da568b2d8f433dfa144f7b27aa788fa8
TEMPONE DE OLIVEIRA Leticia <leticiatdoliveira@gmail.com>
2024-10-23 15:06:24 -0300

    requirements.txt add

--- README.md
+++ README.md
@@ -5,5 +5,21 @@ This project is part of a research on VANT applications in Precision Agriculture
 The goal is to develop and deploy an embedded CNN, using TensorFlowLite, in a Raspberry Pi. 
 The CNN algorithm aims to classify leaf diseases of some plants.
 
-# Author
-[Letícia Tempone de Oliveira](https://www.linkedin.com/in/leticia-tempone/)
+# Set-up
+
+## Packages
+
+Install project requirements.txt in your machine. 
+
+> We recommend a virtual environment use
+
+## Data
+
+We use the [PlantVillage](https://www.kaggle.com/datasets/emmarex/plantdisease) dataset from Kaggle. 
+It's an owned dataset in the community that has been used in a lot of research. 
+
+- Click on the link below of the dataset
+- Download it
+- Unzip files in data/plant-village sub dir of the current project.
+
+For primary development, only tomato data will be used. 
---
+++ src/cnn_plant_diseases.py
@@ -0,0 +1,59 @@
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+# TF settings
+AUTOTUNE = tf.data.AUTOTUNE
+
+# Model hyperparameters
+INIT_LR = 1e-3
+BATCH_SIZE = 16
+EPOCHS = 100
+
+# Image settings param.
+IMAGE_SIZE = 256
+default_image_size = tuple((IMAGE_SIZE, IMAGE_SIZE))
+image_size = 0
+CHANNELS = 3
+
+# Data settings
+dataset_name = 'plant-village'
+user_dir = '/Users/tempone/'
+project_dir = 'Git/IC_vant/embedded-img-classification/'
+data_dir: str = user_dir + project_dir + 'data/' + dataset_name + '/'
+
+
+def show_const_var_settings():
+    print("Printing const settings...")
+    print("\t data_dir: " + data_dir)
+    print("\n")
+
+
+def check_data_dir():
+    if os.path.exists(data_dir):
+        print("Data_dir found !")
+    else:
+        print("The data_dir not found !")
+
+
+def get_dataset_info(dir: str) -> int:
+    dir_path = Path(dir)
+    image_count = len(list(dir_path.glob('*/*.jpg')))
+    image_count += len(list(dir_path.glob('*/*.JPG')))
+    return image_count
+
+
+if __name__ == '__main__':
+    show_const_var_settings()
+
+    # Check dataset infos
+    check_data_dir()
+    nb_img_data = get_dataset_info(data_dir)
+    print(f"# {dataset_name.upper()} dataset contains {nb_img_data} images")
