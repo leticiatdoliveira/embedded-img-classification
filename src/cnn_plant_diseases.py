7166511fc59133127302f1543fdef0c49f4ab21a
TEMPONE DE OLIVEIRA Leticia <leticiatdoliveira@gmail.com>
2024-10-24 15:27:25 -0300

    A working simple CNN

--- src/cnn_plant_diseases.py
+++ src/cnn_plant_diseases.py
@@ -4,26 +4,25 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
 
-# TF settings
-AUTOTUNE = tf.data.AUTOTUNE
 
 # Model hyperparameters
-INIT_LR = 1e-3
 BATCH_SIZE = 16
-EPOCHS = 100
+EPOCHS = 2
 
 # Image settings param.
 IMAGE_SIZE = 256
-default_image_size = tuple((IMAGE_SIZE, IMAGE_SIZE))
-image_size = 0
-CHANNELS = 3
 
 # Data settings
 dataset_name = 'plant-village'
 user_dir = '/Users/tempone/'
 project_dir = 'Git/IC_vant/embedded-img-classification/'
 data_dir: str = user_dir + project_dir + 'data/' + dataset_name + '/'
+
+# Results settings
+results_dir = user_dir + project_dir + 'results/' + dataset_name + '/'
 
 
 def show_const_var_settings():
@@ -37,7 +36,7 @@ def show_const_var_settings():
     print("\t data_dir: " + data_dir)
 
 
-def check_data_dir():
+def check_data_dir(silent_console: bool = True):
     """
     check if the data_dir exists
 
@@ -45,9 +44,11 @@ def check_data_dir():
     :return:
     """
     if os.path.exists(data_dir):
-        print("Data_dir found !")
+        print("Data_dir found !") if not silent_console else None
+        return True
     else:
-        print("The data_dir not found !")
+        print("The data_dir not found !") if not silent_console else None
+        return False
 
 
 def get_dataset_info(directory: str) -> int:
@@ -170,14 +171,16 @@ def load_split_dataset(val_split: float, test_split: float, silent_console: bool
     return train_ds, val_ds
 
 
-def check_dataset_classes(dataset: tf.data.Dataset, dataset_type: str):
+def get_dataset_classes(dataset: tf.data.Dataset, dataset_type: str, silent_console: bool = True):
     """
     check the dataset classes
 
     :return:
     """
     class_names = dataset.class_names
-    print(f"Dataset: {dataset_type} | Nb of classes: {len(class_names)} | Class names: {class_names}")
+    if not silent_console:
+        print(f"Dataset: {dataset_type} | Nb of classes: {len(class_names)} | Class names: {class_names}")
+    return class_names
 
 
 def check_batch_size(dataset: tf.data.Dataset, dataset_type: str):
@@ -186,10 +189,10 @@ def check_batch_size(dataset: tf.data.Dataset, dataset_type: str):
 
     :return:
     """
-    print(f"Checking batch size of the {dataset_type} dataset...")
+    print(f"\n------ Checking batch size of the {dataset_type} dataset...")
     for image_batch, labels_batch in dataset:
         print(f"Image batch shape: {image_batch.shape}")
-        print(f"Label batch shape: {labels_batch.shape}")
+        print(f"Label batch shape: {labels_batch.shape}\n")
         break
 
 
@@ -202,7 +205,7 @@ def display_img_sample_of_dataset(dataset: tf.data.Dataset, dataset_type: str):
     plt.figure(figsize=(10, 10))
     class_names = dataset.class_names
 
-    # Take one batch of images
+    # Take one batch of images and create a subplot
     for images, labels in dataset.take(1):
         for i in range(9):
             ax = plt.subplot(3, 3, i + 1)  # Create the subplot
@@ -217,23 +220,165 @@ def display_img_sample_of_dataset(dataset: tf.data.Dataset, dataset_type: str):
 
     # Add a title to the entire plot
     plt.suptitle(f"Sample of images from the {dataset_type} dataset", fontsize=16)
-    plt.show()
+
+    # Save the plot
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    plt.savefig(results_dir + f"sample_images_{dataset_type}.png")
+
+
+def set_prefetch(dataset: tf.data.Dataset):
+    """
+    set the prefetch for the dataset
+
+    :param dataset: tf.data.Dataset
+    :return: tf.data.Dataset
+    """
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    return dataset.shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+
+
+def normalize_dataset(dataset: tf.data.Dataset):
+    """
+    Normalize image data.
+    RGB values are in the [0, 255] range, so we need to scale them to the [0, 1] range.
+
+    :return:
+    """
+    print("Normalizing dataset...")
+    normalizer_layer = tf.keras.layers.Rescaling(1. / 255)
+    normalized_ds = dataset.map(lambda x, y: (normalizer_layer(x), y))
+    image_batch, labels_batch = next(iter(normalized_ds))
+
+    # get the first image to check the pixel values
+    first_image = image_batch[0]
+    print(f"First image pixel values -> Min: {np.min(first_image)} | Max: {np.max(first_image)} | "
+          f"Shape: {first_image.shape}")
+    return normalized_ds
+
+
+def create_model(class_names: list, img_height: int, img_width: int):
+    """
+    Create a CNN model to classify image
+
+    :return:
+    """
+    num_classes = len(class_names)
+
+    input_shape = (img_height, img_width, 3)
+
+    model = Sequential([
+        layers.Input(shape=input_shape),
+        layers.Rescaling(1. / 255),
+        layers.Conv2D(16, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(32, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Conv2D(64, 3, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(num_classes)
+    ])
+
+    return model
+
+
+def compile_mode(model: tf.keras.Model):
+    """
+    Compile the model
+
+    :return:
+    """
+    model.compile(optimizer='adam',
+                  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    model.summary()
+
+    return model
+
+
+def train_model(model: tf.keras.Model, train_ds: tf.data.Dataset, val_ds: tf.data.Dataset, nb_epochs: int = EPOCHS):
+    """
+    Train the model
+
+    :return:
+    """
+    hist = model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=nb_epochs
+    )
+
+    return hist
+
+
+def show_model_fit(hist: tf.keras.callbacks.History, nb_epochs: int):
+    """
+    Show the model fit
+
+    :return:
+    """
+    acc = hist.history['accuracy']
+    val_acc = hist.history['val_accuracy']
+
+    loss = hist.history['loss']
+    val_loss = hist.history['val_loss']
+
+    epochs_range = range(nb_epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+
+    # Save the plot
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    plt.savefig(results_dir + "model_fit.png")
+
+
+def save_model(model: tf.keras.Model, model_name: str, file_format: str = "keras"):
+    """
+    Save the model
+
+    :return:
+    """
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    model_file = model_name + "." + file_format
+    model_path = results_dir + model_file
+    model.save(model_path)
 
 
 if __name__ == '__main__':
     show_const_var_settings()
 
     # Check dataset infos
-    check_data_dir()
+    if not check_data_dir():
+        raise ValueError("Data dir not found !")
     nb_img_data = get_dataset_info(data_dir)
-    print(f"# {dataset_name.upper()} dataset contains {nb_img_data} images")
+    print(f"# {dataset_name.upper()} dataset contains {nb_img_data} images\n")
 
     # Load split dataset
     train_dataset, val_dataset = load_split_dataset(0.2, 0.2, silent_console=True)
 
     # Check dataset classes
-    check_dataset_classes(train_dataset, "train")
-    check_dataset_classes(val_dataset, "validation")
+    train_classes = get_dataset_classes(train_dataset, "train")
+    val_classes = get_dataset_classes(val_dataset, "validation")
+    if train_classes != val_classes:
+        raise ValueError("The classes in the train and validation datasets are different")
+    else:
+        print(f"Nb of class: {len(train_classes)} | Classes: {train_classes}\n")
 
     # Display sample of images
     check_batch_size(train_dataset, "train")
@@ -242,6 +387,54 @@ if __name__ == '__main__':
     # Display sample of images
     check_batch_size(val_dataset, "validation")
     display_img_sample_of_dataset(val_dataset, "validation")
+
+    # Set prefetch for the dataset
+    print("\n---- Setting prefetch for the dataset...")
+    train_dataset = set_prefetch(train_dataset)
+    val_dataset = set_prefetch(val_dataset)
+
+    # Normalize data
+    print("\n---- Normalizing dataset...")
+    train_dataset_normalized = normalize_dataset(train_dataset)
+    val_dataset_normalized = normalize_dataset(val_dataset)
+
+    # Check if a fit model is already saved
+    if os.path.exists(results_dir + "cnn_first_model.keras"):
+        print("\n---- A model is already saved !")
+        print("\n---- Loading the model...")
+        model = tf.keras.models.load_model(results_dir + "cnn_first_model.keras")
+        model.summary()
+    else:
+        # Build the model
+        print("\n---- Building the model...")
+        model = create_model(train_classes, IMAGE_SIZE, IMAGE_SIZE)
+        model = compile_mode(model)
+
+        # Fit the model
+        print("\n---- Training the model...")
+        hist = train_model(model, train_dataset_normalized, val_dataset_normalized, EPOCHS)
+        show_model_fit(hist, EPOCHS)
+
+        # Save the model
+        print("\n---- Saving the model...")
+        save_model(model, "cnn_first_model", "keras")
+
+    # Evaluate the model
+    print("\n---- Evaluating the model...")
+    print("### Val dataset evaluation:")
+    model.evaluate(val_dataset_normalized)
+
+    print("### Train dataset evaluation:")
+    model.evaluate(train_dataset_normalized)
+
+    # TODO : refactor print to logger
+    # TODO : add test dataset
+    # TODO : add inference function
+
+
+
+
+
 
 
 
